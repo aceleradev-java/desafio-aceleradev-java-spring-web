22b283e79f25bcc6176c9f37d7a5ecd284c46dac
Adriano Avelino <adriano-st777@hotmail.com>
2020-07-23 16:04:35 -0300

    atualiza o endpoint para Acceleration acessar parâmetro spor query string

--- src/main/java/com/challenge/endpoints/AccelerationController.java
+++ src/main/java/com/challenge/endpoints/AccelerationController.java
@@ -6,9 +6,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.challenge.entity.Acceleration;
+import com.challenge.exceptions.ResourceNotFoundException;
 import com.challenge.service.interfaces.AccelerationServiceInterface;
 
 @RestController
@@ -18,19 +20,18 @@ public class AccelerationController {
     @Autowired
     private AccelerationServiceInterface accelerationService;
 
-    @GetMapping
-    public List<Acceleration> findAll() {
-        return this.accelerationService.findAll();
-    }
-
     @GetMapping("/{id}")
     public Acceleration findById(@PathVariable("id") Long id) {
-        return this.accelerationService.findById(1l).get();
-
+        return this.accelerationService.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Acceleration not found"));
     }
 
-    @GetMapping("/company/{companyId}")
-    public List<Acceleration> findByCompany(@PathVariable("companyId") Long companyId) {
+    @GetMapping
+    public List<Acceleration> findByCompany(
+            @RequestParam(name = "companyId", required = true, defaultValue = "0") @PathVariable("companyId") Long companyId) {
+        if (companyId == 0) {
+            return this.accelerationService.findAll();
+        }
         return this.accelerationService.findByCompanyId(companyId);
     }
 

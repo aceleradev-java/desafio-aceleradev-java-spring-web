d86d852463cfaf1f607cc1c68caeb5efa3250c6c
Adriano Avelino <adriano-st777@hotmail.com>
2020-07-23 14:18:48 -0300

    atualiza endpoint para Company para acessar via query string
    Ex: /company?userId=x para acessar as company por id do User ou 
    /company?:accelerationId=x para acessar as company por Acceleration

--- src/main/java/com/challenge/endpoints/CompanyController.java
+++ src/main/java/com/challenge/endpoints/CompanyController.java
@@ -1,14 +1,17 @@
 package com.challenge.endpoints;
 
-import java.util.List;
+import java.util.HashSet;
+import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.challenge.entity.Company;
+import com.challenge.exceptions.ResourceNotFoundException;
 import com.challenge.service.interfaces.CompanyServiceInterface;
 
 @RestController
@@ -20,17 +23,21 @@ public class CompanyController {
 
     @GetMapping("/{id}")
     public Company findById(@PathVariable("id") Long id) {
-        return this.companyService.findById(id).get();
+        return this.companyService.findById(id).orElseThrow(() -> new ResourceNotFoundException("Company not found"));
     }
 
-    @GetMapping("/acceleration/{accelerationId}")
-    public List<Company> findByAccelerationId(@PathVariable("accelerationId") Long accelerationId) {
-        return this.companyService.findByAccelerationId(accelerationId);
-    }
-    
-    @GetMapping("/user/{userId}")
-    public List<Company> finByUserId(@PathVariable("userId") Long userId) {
-        return this.companyService.findByUserId(userId);
+    @GetMapping
+    public Set<Company> findBy(
+            @RequestParam(value = "accelerationId", required = false, defaultValue = "0") Long accelerationId,
+            @RequestParam(value = "userId", required = false, defaultValue = "0") Long userId
+            ) {
+        Set<Company> companies = new HashSet<>();
+        if (accelerationId != 0) {
+            companies.addAll(this.companyService.findByAccelerationId(accelerationId));
+            return companies;
+        }
+        companies.addAll(this.companyService.findByUserId(userId));
+        return companies;
     }
 
 }
